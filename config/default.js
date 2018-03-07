75ac8a0c05e79fafc69153e5dea3ddeab26efc9c
dcry <2507648942@qq.com>
2018-03-07 18:57:00 +0800

    lanyu 3.1

--- config/default.js
+++ config/default.js
@@ -1,9 +1,9 @@
 module.exports = {
-  port: 3000,
+  port: 6000,
   session: {
     secret: 'myblog',
     key: 'myblog',
     maxAge: 2592000000
   },
-  mongodb: 'mongodb://localhost:27017/myblog'
+  mongodb: 'mongodb://huoys:453822@localhost:27017/huoys_db'
 }
